c2aa6d805594cae5f8ce55ac4ec64a78eea7c84b
jmalovera10 <jma.lovera10@uniandes.edu.co>
2019-09-05 21:56:09 -0500

    Endline try

--- BTServer.py
+++ BTServer.py
@@ -13,37 +13,10 @@ class SerialComm:
         self.port = serial.Serial("/dev/rfcomm0", baudrate=9600, timeout=1)
 
     def read_serial(self):
-        res = self.port.read(50)
-        if len(res):
-            return res.splitlines()
-        else:
-            return []
+        return self.port.readline()
 
     def send_serial(self, text):
-        self.port.write(text)
-
-    def is_json(self, mJson):
-        try:
-            json_object = json.loads(mJson)
-            if isinstance(json_object, int):
-                return False
-
-            if len(json_object) == 0:
-                return False
-        except ValueError as e:
-            return False
-        return True
-
-    def isValidCommand(self, command, invalidCommand):
-        if command not in invalidCommand:
-            if re.match("^[a-zA-Z0-9. -]+$", command):
-                return True
-
-        return False
-
-    def readExecuteSend(self, ble_line):
-        json_object = json.loads(ble_line)
-        print "Random: " + str(random.randint(1, 10))
+        self.port.write(text + str('\n'))
 
 
 def main():
