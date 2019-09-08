dd4ce54655d0a71bda5438271f283f13291e7cf9
jmalovera10 <jma.lovera10@uniandes.edu.co>
2019-09-07 20:15:55 -0500

    Identation fix

--- BTServer.py
+++ BTServer.py
@@ -10,40 +10,39 @@ import random
 
 class SerialComm:
     def __init__(self):
-        self.port = serial.Serial("/dev/rfcomm0", baudrate=9600 ,timeout=1)
+        self.port = serial.Serial("/dev/rfcomm0", baudrate=9600, timeout=1)
 
     def read_serial(self):
         return self.port.readline()
 
     def send_serial(self, text):
         self.port.write((text + str('\n')).encode())
-    
+
     def is_json(self, mJson):
-	try:
-	    json_object = json.loads(mJson)
-	    if isinstance(json_object,int):
-		return False
+        try:
+            json_object = json.loads(mJson)
+            if isinstance(json_object, int):
+                return False
 
-	    if len(json_object) == 0:
-		return False
+            if len(json_object) == 0:
+                return False
 
-	except ValueError as e:
-	    return False
-	return True
-
+        except ValueError as e:
+            return False
+        return True
 
 
 class StateManager:
     def __init__(self):
-	self.state = "IDLE"
-    
+        self.state = "IDLE"
+
     def change_state(self, new_state):
-	self.state = new_state
+        self.state = new_state
 
     def manage(self, command, ble_comm):
-	if self.state == "MEASURE":
+        if self.state == "MEASURE":
             if command == "PH":
-		ble_comm.send_serial("PH MEASUREMENT")
+                ble_comm.send_serial("PH MEASUREMENT")
             elif command == "DO":
                 ble_comm.send_serial("DO MEASUREMENT")
             elif command == "T":
@@ -62,12 +61,12 @@ def main():
             out = ble_comm.read_serial()
             for ble_line in out:
                 print(out)
-		if ble_comm.is_json(ble_line):
-		    message = json.loads(ble_line)
-		    state = message['STATE']
-		    command = message['COMMAND']
-		    manager.change_state(state)
-		    manager.manage(command,ble_comm)
+                if ble_comm.is_json(ble_line):
+                    message = json.loads(ble_line)
+                    state = message['STATE']
+                    command = message['COMMAND']
+                    manager.change_state(state)
+                    manager.manage(command, ble_comm)
                     ble_comm.send_serial(ble_line)
 
         except serial.SerialException:
