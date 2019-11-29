559280493a95f315e7a735d76a1d1f42cb6144cc
jmalovera10 <jma.lovera10@uniandes.edu.co>
2019-11-29 16:33:43 -0500

    Emergency use 2

--- managers/sensors/turbidity.py
+++ managers/sensors/turbidity.py
@@ -32,15 +32,16 @@ class TurbidityManager:
         turbidity_angle2 = 0
         for i in range(10):
             # First perspective measurement
-            GPIO.output(27, GPIO.HIGH)
-            voltage_angle1 = self.turbidity_angle1.voltage
-            turbidity_angle1 += voltage_angle1
+            #GPIO.output(27, GPIO.HIGH)
+            #voltage_angle1 = self.turbidity_angle1.voltage
+            #turbidity_angle1 += voltage_angle1
             # turbidity_angle1 += (-594.887612 * voltage_angle1) + 2525.903701
-            GPIO.output(27, GPIO.LOW)
+            # GPIO.output(27, GPIO.LOW)
             # Second perspective measurement
-            #GPIO.output(17, GPIO.HIGH)
-            #voltage_angle2 = self.turbidity_angle2.voltage
+            GPIO.output(17, GPIO.HIGH)
+            voltage_angle2 = self.turbidity_angle2.voltage
+            turbidity_angle2 += voltage_angle2
             #turbidity_angle2 += (-594.887612 * voltage_angle2) + 2525.903701
-            #GPIO.output(17, GPIO.LOW)
-        self.turbidity_value = (turbidity_angle1) / 10
+            GPIO.output(17, GPIO.LOW)
+        self.turbidity_value = (turbidity_angle2) / 10
         return self.turbidity_value, "NTU"
