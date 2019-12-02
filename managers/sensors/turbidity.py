cd69fb83444c0fd51347c0b00dd4676f62d98699
jmalovera10 <jma.lovera10@uniandes.edu.co>
2019-12-02 08:28:49 -0500

    Formula insert for turbidity

--- managers/sensors/turbidity.py
+++ managers/sensors/turbidity.py
@@ -40,8 +40,8 @@ class TurbidityManager:
             # Second perspective measurement
             GPIO.output(17, GPIO.HIGH)
             voltage_angle2 = self.turbidity_angle2.voltage
-            turbidity_angle2 += voltage_angle2
-            #turbidity_angle2 += (-594.887612 * voltage_angle2) + 2525.903701
+            # turbidity_angle2 += voltage_angle2
+            turbidity_angle2 += (-25346.15385 * voltage_angle2) + 97276.36154
             GPIO.output(17, GPIO.LOW)
         self.turbidity_value = (turbidity_angle2) / 20
         return self.turbidity_value, "NTU"
