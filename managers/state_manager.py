8ef47c890b75ea007de4ea687a9eaaa3260b543d
jmalovera10 <jma.lovera10@uniandes.edu.co>
2019-12-08 18:04:52 -0500

    BT Error management

--- managers/state_manager.py
+++ managers/state_manager.py
@@ -13,25 +13,28 @@ class StateManager:
         self.command = command
 
     def manage(self, ble_comm):
-        if self.state == "MEASURE":
-            if self.command == "pH":
-                print("PH SENT")
-                ph = self.sensor_manager.process_ph_measurement()
-                # ble_comm.send_serial('{"NAME":"pH","VALUE":%.3f,"UNITS":"%s"}' % (ph[0], ph[1]))
-                ble_comm.send('{"NAME":"pH","VALUE":%.3f,"UNITS":"%s"}\r\n' % (ph[0], ph[1]))
-            elif self.command == "CONDUCTIVIDAD":
-                print("CONDUCTIVITY SENT")
-                conductivity = self.sensor_manager.process_conductivity_measurement()
-                #ble_comm.send_serial(
-                    #'{"NAME":"CONDUCTIVIDAD","VALUE":%.3f,"UNITS":"%s"}' % (conductivity[0], conductivity[1]))
-                ble_comm.send('{"NAME":"CONDUCTIVIDAD","VALUE":%.3f,"UNITS":"%s"}\r\n' % (conductivity[0], conductivity[1]))
-            elif self.command == "TURBIEDAD":
-                print("TURBIDITY SENT")
-                turbidity = self.sensor_manager.process_turbidity_measurement()
-                # ble_comm.send_serial('{"NAME":"TURBIEDAD","VALUE":%.3f,"UNITS":"%s"}' % (turbidity[0], turbidity[1]))
-                ble_comm.send('{"NAME":"TURBIEDAD","VALUE":%.3f,"UNITS":"%s"}\r\n' % (turbidity[0], turbidity[1]))
-            elif self.command == "BATTERY":
-                print("BATTERY STATUS SENT")
-                battery = self.battery_manager.get_battery_measurement()
-                #ble_comm.send_serial('{"NAME":"BATTERY","VALUE":%d,"UNITS":"%s"}' % (battery[0], battery[1]))
-                ble_comm.send('{"NAME":"BATTERY","VALUE":%d,"UNITS":"%s"}\r\n' % (battery[0], battery[1]))
+        try:
+            if self.state == "MEASURE":
+                if self.command == "pH":
+                    print("PH SENT")
+                    ph = self.sensor_manager.process_ph_measurement()
+                    # ble_comm.send_serial('{"NAME":"pH","VALUE":%.3f,"UNITS":"%s"}' % (ph[0], ph[1]))
+                    ble_comm.send('{"NAME":"pH","VALUE":%.3f,"UNITS":"%s"}\r\n' % (ph[0], ph[1]))
+                elif self.command == "CONDUCTIVIDAD":
+                    print("CONDUCTIVITY SENT")
+                    conductivity = self.sensor_manager.process_conductivity_measurement()
+                    #ble_comm.send_serial(
+                        #'{"NAME":"CONDUCTIVIDAD","VALUE":%.3f,"UNITS":"%s"}' % (conductivity[0], conductivity[1]))
+                    ble_comm.send('{"NAME":"CONDUCTIVIDAD","VALUE":%.3f,"UNITS":"%s"}\r\n' % (conductivity[0], conductivity[1]))
+                elif self.command == "TURBIEDAD":
+                    print("TURBIDITY SENT")
+                    turbidity = self.sensor_manager.process_turbidity_measurement()
+                    # ble_comm.send_serial('{"NAME":"TURBIEDAD","VALUE":%.3f,"UNITS":"%s"}' % (turbidity[0], turbidity[1]))
+                    ble_comm.send('{"NAME":"TURBIEDAD","VALUE":%.3f,"UNITS":"%s"}\r\n' % (turbidity[0], turbidity[1]))
+                elif self.command == "BATTERY":
+                    print("BATTERY STATUS SENT")
+                    battery = self.battery_manager.get_battery_measurement()
+                    #ble_comm.send_serial('{"NAME":"BATTERY","VALUE":%d,"UNITS":"%s"}' % (battery[0], battery[1]))
+                    ble_comm.send('{"NAME":"BATTERY","VALUE":%d,"UNITS":"%s"}\r\n' % (battery[0], battery[1]))
+        except btcommon.BluetoothError:
+            print('Connection lost')
